0ba46cf490fe844e704504bfc4bd97168cd38850
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2022-05-30 04:39:12 +0700

    Relase v1.0.0

---
+++ students.js
@@ -0,0 +1,158 @@
+const students = [
+  {
+    stdID: "15973",
+    fullName: "Noppawan Jearapongpakorn",
+  },
+  {
+    stdID: "15979",
+    fullName: "Panuttha Chantharabut",
+  },
+  {
+    stdID: "15992",
+    fullName: "Warita Kitikul",
+  },
+  {
+    stdID: "15995",
+    fullName: "Onlin Anantananont",
+  },
+  {
+    stdID: "16005",
+    fullName: "Natcha Bunprakasit",
+  },
+  {
+    stdID: "16022",
+    fullName: "WisakhaLaoroekutai",
+  },
+  {
+    stdID: "16029",
+    fullName: "Chawalnuch Pluemarom",
+  },
+  {
+    stdID: "16031",
+    fullName: "Nathathida Thammapala",
+  },
+  {
+    stdID: "16036",
+    fullName: "Thanawan Leartthanongsak",
+  },
+  {
+    stdID: "16055",
+    fullName: "Sarisa Pronanunt",
+  },
+  {
+    stdID: "16056",
+    fullName: "Suchada Kosavisutte",
+  },
+  {
+    stdID: "16068",
+    fullName: "Nunnasorn Keawphongsri",
+  },
+  {
+    stdID: "16075",
+    fullName: "Piriyada Limvatanayingyong",
+  },
+  {
+    stdID: "16077",
+    fullName: "Pakjira Siangsakul",
+  },
+  {
+    stdID: "16082",
+    fullName: "Lattaphan Pisityothin",
+  },
+  {
+    stdID: "16083",
+    fullName: "Vareepittra Suvarnasara",
+  },
+  {
+    stdID: "16205",
+    fullName: "Karinee Srirabheebhat",
+  },
+  {
+    stdID: "16210",
+    fullName: "Natpimol Luckklai",
+  },
+  {
+    stdID: "17077",
+    fullName: "Apichayada Phairachkul",
+  },
+  {
+    stdID: "17636",
+    fullName: "Sarisa NaennaJetkultorn",
+  },
+  {
+    stdID: "17929",
+    fullName: "Thawanrat Khunthanaviroj",
+  },
+  {
+    stdID: "17934",
+    fullName: "Parima Watcharatit",
+  },
+  {
+    stdID: "18221",
+    fullName: "Fahsai Chumteerathum",
+  },
+  {
+    stdID: "18502",
+    fullName: "Paphapinn Nganbunsri",
+  },
+  {
+    stdID: "18503",
+    fullName: "Ramita Chaisrisoponkij",
+  },
+  {
+    stdID: "18505",
+    fullName: "Ploy Feungtaweechoke",
+  },
+  {
+    stdID: "18835",
+    fullName: "Weenarat Boonsirichay",
+  },
+  {
+    stdID: "18836",
+    fullName: "Teemaporn Ongsirithaworn",
+  },
+  {
+    stdID: "18837",
+    fullName: "Paksamon Maneewongvijit",
+  },
+  {
+    stdID: "18838",
+    fullName: "Phurichaya Chitsurawongse",
+  },
+  {
+    stdID: "18839",
+    fullName: "Wanatchaporn Sukcharoen",
+  },
+  {
+    stdID: "18840",
+    fullName: "Nunnaphat Nawatwuttiwong",
+  },
+  {
+    stdID: "18842",
+    fullName: "Pitchayapa Kulkijja",
+  },
+  {
+    stdID: "18843",
+    fullName: "Phunthila Thrupmonchai",
+  },
+  {
+    stdID: "18845",
+    fullName: "Tachakorn Teachaiwong",
+  },
+  {
+    stdID: "18846",
+    fullName: "Chitchanok Phiban",
+  },
+  {
+    stdID: "18847",
+    fullName: "Hana Lee",
+  },
+  {
+    stdID: "18848",
+    fullName: "Napapat Saetae",
+  },
+  {
+    stdID: "18849",
+    fullName: "KandeeJitjang",
+  },
+];
